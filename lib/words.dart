a3db12579706c58c2fe177e23a7eb6fed00ba036
Linive <linive.cristine2@gmail.com>
2020-07-13 17:36:53 -0300

    Adicionando palavras e respostas

---
+++ lib/words.dart
@@ -0,0 +1,122 @@
+List<Map<String, Object>> words = [
+  {
+    'word': 'Migué',
+    'answers': [
+      {'answer': 'Mentira', 'score': 10},
+      {'answer': 'Bagunça', 'score': 0},
+      {'answer': 'Sem dinheiro', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Brenha ',
+    'answers': [
+      {'answer': 'Gravidez', 'score': 0},
+      {'answer': 'Lugar distante', 'score': 10},
+      {'answer': 'Escuridão', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Mangar ',
+    'answers': [
+      {'answer': 'Rir', 'score': 10},
+      {'answer': 'Colher mangas', 'score': 0},
+      {'answer': 'Brigar', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Arengar ',
+    'answers': [
+      {'answer': 'Brigar', 'score': 10},
+      {'answer': 'Complicar', 'score': 0},
+      {'answer': 'Irritar', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Lomba',
+    'answers': [
+      {'answer': 'Lombada eletônica', 'score': 0},
+      {'answer': 'Vermes', 'score': 0},
+      {'answer': 'Coisa engraçada', 'score': 10},
+    ],
+  },
+  {
+    'word': 'Morgado ',
+    'answers': [
+      {'answer': 'Desanimado', 'score': 10},
+      {'answer': 'Azedo', 'score': 0},
+      {'answer': 'Com raiva', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Farrapar',
+    'answers': [
+      {'answer': 'Cair', 'score': 00},
+      {'answer': 'Faltar compromisso', 'score': 10},
+      {'answer': 'Festejar', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Cabuloso',
+    'answers': [
+      {'answer': 'Pessoa esperta', 'score': 00},
+      {'answer': 'Pessoa com muito cabelo', 'score': 0},
+      {'answer': 'Pessoa chata', 'score': 10},
+    ],
+  },
+  {
+    'word': 'Bate entope',
+    'answers': [
+      {'answer': 'Não simpatizar com alguém', 'score': 00},
+      {'answer': 'Lugar cheio', 'score': 0},
+      {'answer': 'Comida ruim', 'score': 10},
+    ],
+  },
+  {
+    'word': 'Bigú',
+    'answers': [
+      {'answer': 'Jogo de bolinhas', 'score': 00},
+      {'answer': 'Carona', 'score': 10},
+      {'answer': 'Grampo de cabelo', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Peidado',
+    'answers': [
+      {'answer': 'Pessoa indginada', 'score': 10},
+      {'answer': 'Pessoa que solta gases', 'score': 0},
+      {'answer': 'Algo sem qualidade', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Bruguelo',
+    'answers': [
+      {'answer': 'Tipo de carro', 'score': 0},
+      {'answer': 'Pessoa branca', 'score': 0},
+      {'answer': 'Criança pequena', 'score': 10},
+    ],
+  },
+  {
+    'word': 'Catenga',
+    'answers': [
+      {'answer': 'Rato', 'score': 0},
+      {'answer': 'Largatixa', 'score': 10},
+      {'answer': 'Barata', 'score': 0},
+    ],
+  },
+  {
+    'word': 'Muganga',
+    'answers': [
+      {'answer': 'Comida', 'score': 0},
+      {'answer': 'Brincadeira', 'score': 0},
+      {'answer': 'Mania', 'score': 10},
+    ],
+  },
+  {
+    'word': 'Encangado',
+    'answers': [
+      {'answer': 'Pessoas que andam juntas', 'score': 10},
+      {'answer': 'Pessoa engraçada', 'score': 0},
+      {'answer': 'Pessoa desconfiada', 'score': 0},
+    ],
+  },
+];
