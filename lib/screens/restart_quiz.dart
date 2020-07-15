1da1033333e2e43cd1741a3dc5c01f7b55acaa1e
Linive <linive.cristine2@gmail.com>
2020-07-15 09:45:17 -0300

    Implementando a rota restart Quiz

--- lib/screens/restart_quiz.dart
+++ lib/screens/restart_quiz.dart
@@ -1,9 +1,30 @@
 import 'package:alagoanes/components/background.dart';
 import 'package:flutter/material.dart';
+import '../main.dart';
 
 class RestartPage extends StatelessWidget {
   final int score;
   RestartPage(this.score);
+
+  String scoreMensageFilter() {
+    if (score < 70) {
+      return 'Ops . . .\n Não foi dessa vez.';
+    } else if (score >= 70 || score <= 100) {
+      return 'Mandou bem!\n Quase melhor que São João.';
+    } else {
+      return 'Me fala a verdade...!\n Tu é nordestino, ne?.';
+    }
+  }
+
+  String socoreImageFilter() {
+    if (score < 70) {
+      return 'assets/images/seca.png';
+    } else if (score >= 70 || score <= 100) {
+      return 'assets/images/festa.png';
+    } else {
+      return 'assets/images/sanfona.png';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +49,12 @@ class RestartPage extends StatelessWidget {
                   width: 85,
                   margin: EdgeInsets.only(bottom: 10, top: 10),
                   child: Image.asset(
-                    'assets/images/festa.png',
+                    socoreImageFilter(),
                     color: Colors.white,
                   ),
                 ),
                 Text(
-                  'Mandou bem!\n Quase melhor que São João.',
+                  scoreMensageFilter(),
                   style: Theme.of(context).textTheme.headline4,
                   textAlign: TextAlign.center,
                 ),
@@ -50,7 +71,14 @@ class RestartPage extends StatelessWidget {
             margin: EdgeInsets.only(top: 20),
             child: FloatingActionButton(
               backgroundColor: Color.fromRGBO(58, 58, 61, .83),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
               child: Icon(
                 Icons.replay,
                 color: Colors.white,
